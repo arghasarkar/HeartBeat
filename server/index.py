3b041311d83c2e9c2f0b9e2d22ae6949f66d5d9a
Argha Sarkar <argha@arghasarkar.co.uk>
2016-11-19 19:57:56 +0000

    feat(file_upload): File uploaded on /upload path (#1)
    - .txt and .csv files can be uploaded to the HOST/upload.
      The files are saved on disk at /uploads
    
    [QA]
    Has tests: No
    Reviewed by: Nil

---
+++ server/index.py
@@ -0,0 +1,48 @@
+from flask import Flask, json, redirect, url_for, request
+import csv, os
+from werkzeug.utils import secure_filename
+
+PATH_TO_UPLOADS = "./uploads"
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = PATH_TO_UPLOADS
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def hello():
+    return "Hello World! Test"
+
+@app.route('/upload', methods = ['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            #flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            #flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
